87262aa986c38aab10edebdc9bb3d2a1482cc7c2
Freddy Marin <freddymarin.jpg@gmail.com>
2022-12-08 11:32:47 -0600

    resume on pc

--- network/static/network/home.jsx
+++ network/static/network/home.jsx
@@ -1,11 +1,15 @@
 const { useEffect, useState } = React;
 
+
+
 const Home = () => {
    const [posts, setPosts] = useState([])
    const [profiles, setProfiles] = useState([])
    const [singleProfile, setSingleProfile] = useState(null)
    const [toggleProfile, setToggleProfile] = useState(false)
    const [currentUser, setCurrentUser] = useState(null)
+
+   
 
    useEffect(() => {
       fetch('/current/')
@@ -37,9 +41,31 @@ const Home = () => {
             <div>
                
                {posts.map((post) => {
-                  const { content, timestamp, creator, likes } = post;
+                  const { id, content, timestamp, creator, likes } = post;
+                  let likeArray = likes;
+                  let likesLength = likes.length;
+                  
+                  let likeHandler = () => {
+                     if(currentUser){
+                        if(likeArray.indexOf(currentUser)===-1){
+                           likesLength++
+                           likeArray.unshift(currentUser)
+                           console.log(likeArray)
+                           console.log(likesLength);
+                        } else {
+                           likesLength--
+                           let newArray = likeArray.filter(user => user !== currentUser)
+                           likeArray = newArray
+                           console.log(newArray)
+                           console.log(likesLength);
+                        }
+                     }else{
+                        console.log("sign in first")
+                     }
+                  }
+
                   return (
-                     <div class="tweet-wrap">
+                     <div class="tweet-wrap" key={id}>
                         <div class="tweet-header">
                            <div class="tweet-header-info">
                               <div className="creator" onClick={() => profileHandler(creator)}>
@@ -51,9 +77,9 @@ const Home = () => {
                         </div>
                         <div class="tweet-info-counts">
                            <div class="likes">
-                              <svg class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                              <div class="likes-count">
-                                 {likes.length}
+                              <svg onClick ={()=>likeHandler()} id="likeBtn" class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                              <div id="likes-count" class="likes-count">
+                                 {likesLength}
                               </div>
                            </div>
                         </div>
