39e83279041b5d715774d69772e108988adbe0af
Freddy Marin <freddymarin.jpg@gmail.com>
2022-12-08 13:43:52 -0600

    added like and unlike styling

--- network/static/network/Tweet.jsx
+++ network/static/network/Tweet.jsx
@@ -3,8 +3,9 @@ const { useEffect, useState } = React;
 const Tweet = ({ content, timestamp, creator, likes, profiles, setSingleProfile, setToggleProfile, currentUser }) => {
 
     let likeArray = likes;
-    let [likesLength, setLikesLength] = useState(likeArray.length)
-    // let [likesLength, setLikesLength] = useState(likeArray.length);
+    const [likesLength, setLikesLength] = useState(likeArray.length)
+
+    const [fill, setFill] = useState(false)
 
     const profileHandler = (creator) => {
         for (const profile of profiles) {
@@ -21,18 +22,26 @@ const Tweet = ({ content, timestamp, creator, likes, profiles, setSingleProfile,
         if(likeArray.indexOf(currentUser)===-1){
             likeArray.unshift(currentUser)
             setLikesLength(likeArray.length)
-            console.log(likeArray)
+            setFill(true)
         } else {
-            // setLikesLength(likesLength - 1)
             let newArray = likeArray.filter(user => user !== currentUser)
             likeArray = newArray
             setLikesLength(likeArray.length)
-            console.log(newArray)
+            setFill(false)
         }
         }else{
         console.log("sign in first")
         }
     }
+
+    useEffect(()=>{
+        if(likeArray.indexOf(currentUser)===-1){
+            setFill(false)
+        } else {
+            setFill(true)
+        }
+    }, [])
+
     return (
         <div class="tweet-wrap">
             <div class="tweet-header">
@@ -46,7 +55,7 @@ const Tweet = ({ content, timestamp, creator, likes, profiles, setSingleProfile,
             </div>
             <div class="tweet-info-counts">
                 <div class="likes">
-                    <svg onClick ={() => likeHandler()} id="likeBtn" class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                        <svg onClick ={likeHandler} id="likeBtn" class={fill ? "like feather feather-heart sc-dnqmqq jxshSx": "unlike feather feather-heart sc-dnqmqq jxshSx"} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                     <div id="likes-count" class="likes-count">
                         {likesLength}
                     </div>
