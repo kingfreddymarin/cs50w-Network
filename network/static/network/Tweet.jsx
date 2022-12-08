b8736935d2764b34e08272a46b817caa052d3723
Freddy Marin <freddymarin.jpg@gmail.com>
2022-12-08 12:27:38 -0600

    add and remove like set

---
+++ network/static/network/Tweet.jsx
@@ -0,0 +1,57 @@
+const { useEffect, useState } = React;
+
+const Tweet = ({ content, timestamp, creator, likes, profiles, setSingleProfile, setToggleProfile, currentUser }) => {
+
+    let likeArray = likes;
+    let [likesLength, setLikesLength] = useState(likeArray.length)
+    // let [likesLength, setLikesLength] = useState(likeArray.length);
+
+    const profileHandler = (creator) => {
+        for (const profile of profiles) {
+            if (profile.user === creator) {
+                setSingleProfile(profile);
+                setToggleProfile(true);
+            }
+        }
+        document.querySelector("#tweetbox").style.display = "none";
+    }
+    
+    let likeHandler = () => {
+        if(currentUser){
+        if(likeArray.indexOf(currentUser)===-1){
+            likeArray.unshift(currentUser)
+            setLikesLength(likeArray.length)
+            console.log(likeArray)
+        } else {
+            // setLikesLength(likesLength - 1)
+            let newArray = likeArray.filter(user => user !== currentUser)
+            likeArray = newArray
+            setLikesLength(likeArray.length)
+            console.log(newArray)
+        }
+        }else{
+        console.log("sign in first")
+        }
+    }
+    return (
+        <div class="tweet-wrap">
+            <div class="tweet-header">
+                <div class="tweet-header-info">
+                    <div className="creator" onClick={() => profileHandler(creator)}>
+                        {creator} <span>@{creator}</span><span>. {timestamp}
+                        </span>
+                    </div>
+                    <p>{content}</p>
+                </div>
+            </div>
+            <div class="tweet-info-counts">
+                <div class="likes">
+                    <svg onClick ={() => likeHandler()} id="likeBtn" class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                    <div id="likes-count" class="likes-count">
+                        {likesLength}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- network/static/network/home.jsx
+++ network/static/network/home.jsx
@@ -1,15 +1,11 @@
 const { useEffect, useState } = React;
 
-
-
 const Home = () => {
    const [posts, setPosts] = useState([])
    const [profiles, setProfiles] = useState([])
    const [singleProfile, setSingleProfile] = useState(null)
    const [toggleProfile, setToggleProfile] = useState(false)
    const [currentUser, setCurrentUser] = useState(null)
-
-   
 
    useEffect(() => {
       fetch('/current/')
@@ -23,68 +19,23 @@ const Home = () => {
       .then((data) => setProfiles(data))
    }, []);
 
-   const profileHandler = (creator, e) => {
-      for (const profile of profiles) {
-         if (profile.user === creator) {
-            setSingleProfile(profile);
-            setToggleProfile(true);
-         }
-      }
-      document.querySelector("#tweetbox").style.display = "none";
-   }
-
    return (
       <div >
-         {toggleProfile && <Profile profile={singleProfile} closeProfile={setToggleProfile} posts={posts} setPosts={setPosts} currentUser={currentUser}/>}
+         {toggleProfile && <Profile profile={singleProfile} 
+                                    closeProfile={setToggleProfile} 
+                                    posts={posts} 
+                                    setPosts={setPosts} 
+                                    currentUser={currentUser}/>}
          {!toggleProfile && (
             
             <div>
                
                {posts.map((post) => {
-                  const { id, content, timestamp, creator, likes } = post;
-                  let likeArray = likes;
-                  let likesLength = likes.length;
-                  
-                  let likeHandler = () => {
-                     if(currentUser){
-                        if(likeArray.indexOf(currentUser)===-1){
-                           likesLength++
-                           likeArray.unshift(currentUser)
-                           console.log(likeArray)
-                           console.log(likesLength);
-                        } else {
-                           likesLength--
-                           let newArray = likeArray.filter(user => user !== currentUser)
-                           likeArray = newArray
-                           console.log(newArray)
-                           console.log(likesLength);
-                        }
-                     }else{
-                        console.log("sign in first")
-                     }
-                  }
-
-                  return (
-                     <div class="tweet-wrap" key={id}>
-                        <div class="tweet-header">
-                           <div class="tweet-header-info">
-                              <div className="creator" onClick={() => profileHandler(creator)}>
-                                 {creator} <span>@{creator}</span><span>. {timestamp}
-                                 </span>
-                              </div>
-                              <p>{content}</p>
-                           </div>
-                        </div>
-                        <div class="tweet-info-counts">
-                           <div class="likes">
-                              <svg onClick ={()=>likeHandler()} id="likeBtn" class="feather feather-heart sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
-                              <div id="likes-count" class="likes-count">
-                                 {likesLength}
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  )
+                  return <Tweet key={post.id} {...post} 
+                                 profiles={profiles} 
+                                 setSingleProfile={setSingleProfile} 
+                                 setToggleProfile={setToggleProfile}
+                                 currentUser={currentUser}/>
                })}
             </div>
          )}
